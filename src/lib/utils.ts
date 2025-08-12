1fe790d8cbc98459778b21aa03eb918cf3a93a9c
Tyrese-Mv <musawenkosi106@gmail.com>
2025-08-12 14:55:24 +0200

    Enhance utils.ts: format cn function for consistency, add userDB type definition, and introduce Post type for improved data structure management.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,14 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  	return twMerge(clsx(inputs))
 }
 
 export const userDB: { email: string; passwordHash: string }[] = [];
+export type Post = {
+    PK: string;
+    SK: string;
+    content: string;
+    username: string;
+    timestamp?: number;
+};
